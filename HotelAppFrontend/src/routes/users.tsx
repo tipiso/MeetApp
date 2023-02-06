5fcf689ec697342503f0b032e2fd81ad087be655
artur.sokalski <artur.sokalski@zaven.pl>
2023-02-06 17:40:41 +0100

    added frontend

---
+++ HotelAppFrontend/src/routes/users.tsx
@@ -0,0 +1,10 @@
+import { A } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Users() {
+    return (
+        <main class="text-center mx-auto text-gray-700 p-4">
+            <div>Users</div>
+        </main>
+    );
+}
