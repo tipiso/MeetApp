480fcb77652a7bf2d926b9ccd3add47bb55a4faf
artur.sokalski <artur.sokalski@zaven.pl>
2023-02-08 17:27:12 +0100

    added validation to account endpoints

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
 	public class RegisterDto
 	{
+		[Required]
 		public string Username { get; set; }
-		public string Password { get; set; }
+        [Required]
+        public string Password { get; set; }
 	}
 }
 
