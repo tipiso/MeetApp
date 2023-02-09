30bd33ea97a486ab74ce1ceee1c4864ccc651896
artur.sokalski <artur.sokalski@zaven.pl>
2023-02-09 13:46:11 +0100

    initial next commit

---
+++ HotelAppFrontend/src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return <nav><Link href="/">Home</Link><Link href="/users">Users</Link></nav>
+}
--- HotelAppFrontend/src/pages/users.tsx
+++ HotelAppFrontend/src/pages/users.tsx
@@ -1,5 +1,17 @@
+import {ReactElement} from "react";
+import Layout from "@/components/layouts/Layout";
+import Home from "@/pages/index";
+
 const UsersPage = () => {
-    return <></>
+    return <>Users</>
+}
+
+UsersPage.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
 }
 
 export default UsersPage;
