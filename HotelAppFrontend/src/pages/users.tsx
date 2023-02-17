af326786583b120dcc5f045fb1434dd5594f904e
tipiso <tipiso@gmail.com>
2023-02-17 14:33:29 +0100

    test display for users

--- HotelAppFrontend/src/pages/users.tsx
+++ HotelAppFrontend/src/pages/users.tsx
@@ -9,11 +9,24 @@ const UsersPage = () => {
   const [users, setUsers] = useState([]);
   const session = useSession();
 
+  const getUsers = async () => {
+    const { data } = await axios.get(usersUrl, {
+      headers: {
+        Authorization: `Bearer ${session.data?.accessToken}`,
+      },
+    });
+    setUsers(data);
+  };
+
   useEffect(() => {
-    axios.get(usersUrl, { headers: { Authorization: `Bearer ${session.data?.accessToken}` } });
+    getUsers();
   }, []);
 
-  return <>Users</>;
+  return (
+    <>
+      <ul>{users && users.length && users.map((user) => <li>{user.userName}</li>)}</ul>
+    </>
+  );
 };
 
 UsersPage.getLayout = function getLayout(page: ReactElement) {
