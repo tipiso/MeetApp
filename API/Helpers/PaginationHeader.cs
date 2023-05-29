e8e22cbb97ada49c8739698539c3c14ef7cb9f84
artur.sokalski <artur.sokalski@zaven.pl>
2023-05-29 16:49:55 +0200

    added pagination classes

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace API.Helpers
+{
+	public class PaginationHeader
+	{
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPage)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerPage;
+            TotalItems = totalItems;
+            TotalPage = totalPage;
+        }
+
+        public int  CurrentPage { get; set; }
+		public int ItemsPerPage { get; set; }
+		public int TotalItems { get; set; }
+		public int TotalPage { get; set; }
+	}
+}
+
