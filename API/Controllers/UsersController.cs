3c0bdcb57a97aa3d08492e581ac2a9d507896560
artur.sokalski <artur.sokalski@zaven.pl>
2023-05-31 12:54:45 +0200

    added pagination class for controllers universal use

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -26,9 +27,11 @@ namespace API.Controllers
         }
 
 		[HttpGet]
-		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+		public async Task<ActionResult<PagedList<AppUser>>> GetUsers([FromQuery]UserParams userParams)
 		{
-			var users = await _userRepository.GetMembersAsync();
+			var users = await _userRepository.GetMembersAsync(userParams);
+
+			Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
 
 			return Ok(users);
 		}
