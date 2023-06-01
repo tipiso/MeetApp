4210df60a8698d12103275432512912d91b24d51
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-01 15:26:03 +0200

    added initial likes setup

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,38 @@
+
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _userRepository = userRepository;
+            _likesRepository = likesRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.UserName == username) return BadRequest("You cannot like yourself");
+
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+
+            return Ok(new UserLike { SourceUserId = sourceUserId, TargetUserId = likedUser.Id });
+        }
+    }
+}
