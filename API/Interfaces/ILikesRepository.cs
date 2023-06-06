87ca323d7515b392e32f1a5161bf7aba1ab89896
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-06 13:53:44 +0200

    added pagination to likes

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,6 +1,7 @@
 ﻿
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
@@ -8,8 +9,7 @@ namespace API.Interfaces
 	{
 		Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
 		Task<AppUser> GetUserWithLikes(int userId);
-		Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
-		
+		Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
 	}
 }
 
