c74cf7c1b77bf41eeb9fcf605f278b4136c12cde
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-16 16:34:47 +0200

    added asp identity to replace custom auth

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,13 +30,7 @@ namespace API.Controllers
 
 			var user = _mapper.Map<AppUser>(registerDto);
 
-			using var hmac = new HMACSHA512();
-
-
 			user.UserName = registerDto.Username.ToLower();
-			user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-			user.PasswordSalt = hmac.Key;
-			
 
 			_context.Users.Add(user);
 			await _context.SaveChangesAsync();
@@ -55,16 +49,6 @@ namespace API.Controllers
 			var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
 
 			if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-			var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-			for (int i = 0; i < computedHash.Length; i++)
-			{
-
-				if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-			}
 
             return new UserDto
             {
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using API.Data;
 
 namespace API.Extensions
 {
@@ -9,6 +12,14 @@ namespace API.Extensions
 	{
 		public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
 		{
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
