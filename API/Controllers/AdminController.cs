a11c9fe0e1dc1b223a600fce96e1371cdd3b9848
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-21 11:02:22 +0200

    finished up with roles for now

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,16 +1,61 @@
-﻿using API.Enums;
+﻿using API.Entities;
+using API.Enums;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
 	public class AdminController : BaseApiController
 	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AdminController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
 		[Authorize(Policy = Policies.RequireAdmin)]
 		[HttpGet("users-with-roles")]
-		public ActionResult GetUsersWithRoles()
+		public async Task<ActionResult> GetUsersWithRoles()
 		{
-			return Ok("Only admin can see this");
+			var users = await _userManager.Users.
+				OrderBy(u => u.UserName)
+				.Select(u => new
+				{
+					u.Id,
+					Username = u.UserName,
+					Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+				})
+				.ToListAsync();
+
+			return Ok(users);
+		}
+
+		[Authorize(Policy = Policies.RequireAdmin)]
+		[HttpPost("edit-role/{username}")]
+		public async Task<ActionResult> EditRoles(string username, [FromQuery]string roles)
+		{
+			if (string.IsNullOrEmpty(roles)) return BadRequest("You must select a role");
+
+			var selectedRoles = roles.Split(",").ToArray();
+
+			var user = await _userManager.FindByNameAsync(username);
+
+			if (user == null) return NotFound();
+
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+			if (!result.Succeeded) return BadRequest("Failed to add roles");
+
+			result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+			if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+			return Ok(await _userManager.GetRolesAsync(user));
 		}
 
 		[Authorize(Policy = Policies.ModeratePhoto)]
