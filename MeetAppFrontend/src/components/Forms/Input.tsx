cb9489f196003ac6895fbd82aa4a4f3999ad9c1e
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-23 14:37:28 +0200

    added radix for a11y and configured first input and form

--- MeetAppFrontend/src/components/Forms/Input.tsx
+++ MeetAppFrontend/src/components/Forms/Input.tsx
@@ -1,8 +1,10 @@
 import { InputHTMLAttributes } from 'react';
 import cx from 'classnames';
+import * as Form from '@radix-ui/react-form';
 import { Controller, useFormContext } from 'react-hook-form';
+import Label from '@/components/Forms/Label';
 
-type Props = { name: string } & InputHTMLAttributes<HTMLInputElement>;
+type Props = { name: string; label?: string } & InputHTMLAttributes<HTMLInputElement>;
 export default function Input(props: Props) {
   const { control } = useFormContext();
 
@@ -12,16 +14,23 @@ export default function Input(props: Props) {
       control={control}
       name={props.name}
       render={({ field, fieldState }) => (
-        <div>
-          <input
-            {...field}
-            className={cx(
-              props.className,
-              'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
-            )}
-          />
-          {fieldState.error && <div className="text-red-600">{fieldState.error.message}</div>}
-        </div>
+        <Form.FormField name={props.name}>
+          <div>
+            {props.label && <Label text={props.label} />}
+            <Form.Control asChild>
+              <input
+                {...field}
+                type={props.type}
+                className={cx(
+                  props.className,
+                  'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                )}
+              />
+            </Form.Control>
+
+            {fieldState.error && <Form.Message className="text-red-600">{fieldState.error.message}</Form.Message>}
+          </div>
+        </Form.FormField>
       )}
     />
   );
