8c7bbf3b4ff77ab399686991ad6dd4e13232b60b
artur.sokalski <artur.sokalski@zaven.pl>
2023-06-27 13:54:30 +0200

    added flag for checking presence

--- MeetAppFrontend/src/components/UserCard.tsx
+++ MeetAppFrontend/src/components/UserCard.tsx
@@ -1,18 +1,25 @@
 import { User } from '@/types/users';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { OnlineUsersContext } from '@/utils/SignalRProvider';
 
 type Props = {
   user: User;
 };
 export default function UserCard({ user }: Props) {
+  const onlineUsersContext = useContext(OnlineUsersContext);
+  const isOnline = onlineUsersContext.includes(user.userName);
+
   return (
     <Link as="div" href="#" className="card w-80 bg-base-100 shadow-xl">
       <figure>
         <Image src={user.photoUrl} alt={`User ${user.userName} Image`} width={200} height={300} />
       </figure>
       <div className="card-body">
-        <h2 className="card-title mx-auto">{user.userName}</h2>
+        <h2 className="card-title mx-auto">
+          {isOnline ? 'Online' : 'Offline'} {user.userName}
+        </h2>
       </div>
     </Link>
   );
