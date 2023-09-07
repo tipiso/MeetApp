922976c7138b16cd2f5aad542e2d697eb2dc955f
artur.sokalski <artur.sokalski@zaven.pl>
2023-09-07 15:29:07 +0200

    added initial implementation of hobby tags

---
+++ API/Data/HobbiesRepository.cs
@@ -0,0 +1,28 @@
+﻿using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+	public class HobbiesRepository : IHobbiesRepository
+	{
+		public HobbiesRepository()
+		{
+		}
+
+        public void AddHobby(Hobby hobby)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHobby(Hobby hobby)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Hobby>> GetHobbies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ API/Entities/Hobby.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("Hobbies")]
+	public class Hobby
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
+
