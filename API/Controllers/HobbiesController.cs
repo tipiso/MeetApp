803489c4344bc6250cd8b71b7561c3612d9d2716
artur.sokalski <artur.sokalski@zaven.pl>
2023-09-19 16:24:42 +0200

    added hobbies get endpoint

---
+++ API/Controllers/HobbiesController.cs
@@ -0,0 +1,25 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class HobbiesController : BaseApiController
+	{
+        private readonly IUnitOfWork _uow;
+
+        public HobbiesController(IUnitOfWork uow)
+		{
+			_uow = uow;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Hobby>>> GetHobbies()
+		{
+			return Ok(await _uow.HobbiesRepository.GetHobbies());
+		}
+	}
+}
+
