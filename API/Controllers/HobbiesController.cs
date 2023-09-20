3795f6e47c859dbe200a3fc49fd640eb805e7a85
artur.sokalski <artur.sokalski@zaven.pl>
2023-09-20 15:13:23 +0200

    added test implementation of hobbies update

--- API/Controllers/HobbiesController.cs
+++ API/Controllers/HobbiesController.cs
@@ -20,6 +20,36 @@ namespace API.Controllers
 		{
 			return Ok(await _uow.HobbiesRepository.GetHobbies());
 		}
-	}
+
+        [HttpPut("{username}")]
+        public async Task<ActionResult<List<Hobby>>> UpdateUserHobbies(string username, int[] hobbies)
+        {
+            if (hobbies.Length == 0) return BadRequest();
+
+            var user = await _uow.UserRepository.GetUserByUsernameAsync(username);
+
+            var unselectedHobbies = user.UserHobbies.Where(u => !hobbies.Contains(u.HobbyId));
+            var selectedHobbies = hobbies.Where(h => !user.UserHobbies.Select(u => u.HobbyId).Contains(h));
+
+            if (unselectedHobbies.Any())
+            {
+                // Remove those which were removed
+                foreach (UserHobby hobby in unselectedHobbies)
+                {
+                    user.UserHobbies.Remove(hobby);
+                }
+            }
+
+            if (selectedHobbies.Any())
+            {
+                foreach (int hobby in selectedHobbies)
+                {
+                    user.UserHobbies.Add(new UserHobby { HobbyId = hobby, UserId = user.Id });
+                }
+            }
+
+            return Ok(await _uow.HobbiesRepository.GetHobbies());
+        }
+    }
 }
 
