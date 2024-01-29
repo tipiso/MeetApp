d5bdbeab2f532554181d810db773f2fe5462001a
artur.sokalski <artur.sokalski@zaven.pl>
2024-01-29 13:13:00 +0100

    feature: Added date to message box

--- MeetAppFrontend/src/features/messages/components/NavbarMessagesBox.tsx
+++ MeetAppFrontend/src/features/messages/components/NavbarMessagesBox.tsx
@@ -3,18 +3,23 @@ import { Message } from '@/features/messages/types';
 import Avatar from '@/features/users/components/Avatar';
 import { useGetUserMessages } from '..';
 import Loader, { LoaderSizes } from '@/components/Loader';
+import { getDateAndTimeFromDate } from '@/utils/parsers';
 
 type Props = {
   message: Message;
 };
 
 function MessageInbox({ message }: Props) {
+  const date = getDateAndTimeFromDate(message.messageSent);
   return (
     <li>
       <div className="flex items-center px-4 pb-2">
         <Avatar imgUrl={message.senderPhotoUrl} name={message.recipientUsername} minWidth={60} width={60} height={60} />
-        <div className="ml-2 min-w-0 flex-col">
-          <span className="font-bold">{message.senderUsername}</span>
+        <div className="ml-2 min-w-0 flex-grow flex-col">
+          <div className="flex items-center justify-between">
+            <span className="font-bold">{message.senderUsername}</span>
+            <span className="text-xs">{date.dateString}</span>
+          </div>
           <p className="min-w-0 max-w-full truncate text-sm">{message.content}</p>
         </div>
       </div>
@@ -26,9 +31,9 @@ export default function NavbarMessagesBox() {
   const { isLoading, data } = useGetUserMessages();
 
   return (
-    <ul className="dropdown-content menu rounded-box z-10 mt-4 h-96 w-80 bg-base-100 px-0 pt-0 shadow">
+    <ul className="dropdown-content menu rounded-box z-10 mt-4 h-96 w-80 flex-nowrap overflow-auto bg-base-100 px-0 py-0 shadow">
       {isLoading && <Loader size={LoaderSizes.lg} />}
-      {data && data.map((m) => <MessageInbox message={m} />)}
+      {data && data.map((m) => <MessageInbox key={m.id} message={m} />)}
     </ul>
   );
 }
